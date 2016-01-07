7569d931743a22ba9d673b3a41a7c659e4d8a9ce
Jason Underhill <jay.underhill@gmail.com>
2016-01-07 23:25:14 +0000

    setting up mappings

--- Learning.AutoMapper/Global.asax.cs
+++ Learning.AutoMapper/Global.asax.cs
@@ -5,6 +5,9 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using AutoMapper;
+using Learning.AutoMapper.Models;
+using Learning.AutoMapper.Repository;
 
 namespace Learning.AutoMapper
 {
@@ -16,6 +19,8 @@ namespace Learning.AutoMapper
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            Mapper.Initialize(cfg => cfg.CreateMap<Person, PersonViewModel>().ForMember(dest => dest.ConfirmEmail, source => source.MapFrom(x => x.Email)));
         }
     }
 }
